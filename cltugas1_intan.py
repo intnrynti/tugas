f9f56dab6b56480827da3466eaae060d9cac568c
intnrynti <intnrynt@gmail.com>
2025-10-22 14:33:36 +0700

    Create cltugas1_intan.py

---
+++ cltugas1_intan.py
@@ -0,0 +1,33 @@
+class alatmusik :
+     pass
+     
+alatmusik1 = alatmusik()
+alatmusik2 = alatmusik()
+
+print(type(alatmusik1))
+print(type(alatmusik2))
+
+alatmusik1 = alatmusik()
+alatmusik1.nama = "kendang" 
+alatmusik1.jenis = "perkusi"
+alatmusik1.bahan = "kayu & kulit"
+alatmusik1.cara_main = "dipukul"
+alatmusik1.asal = "Jawa Barat"
+
+alatmusik2 = alatmusik()
+alatmusik2.nama = "suling" 
+alatmusik2.jenis = "tiup" 
+alatmusik2.bahan = "bambu"
+alatmusik2.cara_main = "ditiup"
+alatmusik2.asal = "Jawa Barat"
+
+print(alatmusik1.nama)
+print(alatmusik1.jenis)
+print(alatmusik1.bahan)
+print(alatmusik1.cara_main)
+print(alatmusik1.asal)
+print(alatmusik2.nama)
+print(alatmusik2.jenis)
+print(alatmusik2.bahan)
+print(alatmusik2.cara_main)
+print(alatmusik2.asal)
